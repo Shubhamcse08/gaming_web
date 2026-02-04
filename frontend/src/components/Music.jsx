f4de7f362afd0e38c07083aa785aaa6b34e8db72
Shubham <shubhamcse.eng@gmail.com>
2026-02-04 11:53:56 +0530

    Initial commit

---
+++ frontend/src/components/Music.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react"
+
+
+
+const Music = () => {
+    const [isAudioPlaying,setIsAudioPlaying]=useState(false)
+    const [isIndicatorActive,setIsIndicatorActive]=useState(false)
+    
+    const audioElementRef = useRef(null)
+
+    const toggleAudioIndicator =()=>{
+        setIsAudioPlaying((prev)=> !prev)
+        setIsIndicatorActive((prev)=> !prev)
+    }
+
+    useEffect(()=>{
+        if(isAudioPlaying){
+            audioElementRef.current.play()
+        }else{
+            audioElementRef.current.pause()
+        }
+    },[isAudioPlaying])
+  return (
+    <div>
+        <button 
+            onClick={toggleAudioIndicator}
+            className="ml-10 flex items-center space-x-0.5">
+                
+                <audio ref={audioElementRef} 
+                className="hidden" src="/audio/loop.mp3" 
+                loop
+                />
+                    {[1,2,3,4].map((bar)=>(
+                        <div key={bar} 
+                        className={`indicator-line ${isIndicatorActive ? 'active' : ''}`}
+                        style={{animationDelay: `${bar * 0.1}s`}}
+                        />        
+                    ))}
+
+                
+
+        </button>
+    </div>
+  )
+}
+
+export default Music
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from "react"
+import Button from "./Button"
+import { TiLocationArrow } from "react-icons/ti"
+import Music from "./Music"
+import { useWindowScroll} from 'react-use'
+import gsap from 'gsap'
+
+const navItems=['Nexus','Vault','Prologue','About','Contact']
+
+const Navbar = () => {
+    const navContainerRef =useRef(null)
+    const [lastScrollY,setLastScrollY]=useState(0)
+    const [isNavVisble , setIsNavVisble]=useState(true)
+
+    const {y: currentScrollY} = useWindowScroll();
+
+    useEffect(()=>{
+        if(currentScrollY===0){
+            setIsNavVisble(true)
+            navContainerRef.current.classList.remove('floating-nav')
+        }
+        else if(currentScrollY > lastScrollY){
+            setIsNavVisble(false)
+            navContainerRef.current.classList.add('floating-nav')
+        }else if(currentScrollY < lastScrollY){
+            setIsNavVisble(true)
+            navContainerRef.current.classList.add('floating-nav')
+        }
+
+        setLastScrollY(currentScrollY)
+    },[currentScrollY,lastScrollY])
+
+    useEffect(()=>{
+        gsap.to(navContainerRef.current,{
+            y:isNavVisble? 0 : -100,
+            opacity: isNavVisble ? 1:0 ,
+            duration: 0.2,
+        })
+    },[isNavVisble])
+
+  return (
+    <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
+        <header className="absolute top-1/2 w-full -translate-y-1/2 ">
+            <nav className="flex size-full items-center justify-between p-4">
+                <div className="flex items-center gap-7">
+                    <img src="/img/logo.png" alt="Logo" className="w-10"/>
+                
+                    <Button 
+                        id="product-button"
+                        title="Products"
+                        rightIcon={<TiLocationArrow />}
+                        containerClass='bg-blue-50 md:flex hidden items-center justify-center gap-1'
+                    />
+
+                </div>
+                <div className="flex h-full items-center gap-4">
+                    <div className="hidden md:block">
+                        {navItems.map((item)=>(
+                            <a key={item} href={`#${item.toLocaleLowerCase()}`} 
+                                className="nav-hover-btn" >
+                                {item}
+                            </a>
+                        ))}
+                    </div>
+
+                    <Music />
+                </div>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Navbar
